73ebfc8a15f861d30965d57aabb0fcc927e30357
Shah Rukh <shah.xhtml@yahoo.com>
2023-03-25 17:07:45 +0530

    add: full reload for non vue pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
+import FullReload from 'vite-plugin-full-reload'
 
 export default defineConfig({
   plugins: [
     vue(),
     RubyPlugin(),
+    FullReload(['config/routes.rb', 'app/views/**/*'])
   ],
 })
